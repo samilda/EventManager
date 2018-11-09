6982aba3ded72125385870de31e99680080a5916
MOHRI Salem <salem-mohri@hotmail.fr>
2018-11-09 11:04:41 +0100

    SECOND COMMIT

--- app/src/main/java/com/example/mohrisalem/eventmanager/DatabaseManager.java
+++ app/src/main/java/com/example/mohrisalem/eventmanager/DatabaseManager.java
@@ -3,6 +3,9 @@ package com.example.mohrisalem.eventmanager;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+import java.util.Date;
 
 public class DatabaseManager extends SQLiteOpenHelper {
 
@@ -21,12 +24,25 @@ public class DatabaseManager extends SQLiteOpenHelper {
                       + "   event string not null,"
                       + "   when_ integer not null"
                       + ")";
+        db.execSQL( strSql );
+        Log.i( "DATABASE", "onCreate invoker");
 
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        //String strSql = "alter table T_event add column ...";
+        String strSql = "drop table T_event";
+        db.execSQL( strSql );
+        this.onCreate( db );
+        Log.i( "DATABASE", "onUpgrade invoker");
+    }
 
+    public void insertEvent( String name, String event){
+        name = name.replace("'", "''");
+        String strSql = "insert into T_event (name, event, when_) values('"
+                      + name + "', " + event + "," + new Date().getTime() + ")";
+        
     }
 
 }
